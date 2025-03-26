950f5acc9b6bf2d4673a7cb5fb13c834bf82d495
HOTDOG <jhaybiedelossantos19@gmail.com>
2025-03-26 16:04:26 +0800

    update

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function changeTableStyle() {
     let table = document.getElementById("studentTable");
+    
+
+    if (!table) {
+        console.log("Table not found");
+        return;
+    }
+
+
     table.style.border = "2px solid red";
     table.style.backgroundColor = "yellow";
 
+ 
     let ths = table.getElementsByTagName("th");
     for (let th of ths) {
         th.style.backgroundColor = "red";
         th.style.color = "white";
     }
+
 
     let tds = table.getElementsByTagName("td");
     for (let td of tds) {
